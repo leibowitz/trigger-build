9cf2d1ce8eb9f4a02a27d18ae513ea746b52cd4a
Gianni Moschini <leibowitz@users.noreply.github.com>
2018-05-20 16:05:48 +0100

    Remove load_kube_config in lambda caller

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,6 @@
 import json
 import sys
 
-from kubernetes import client, config, watch
 from k8s import k8s_deploy
 
 def deploy(registry, image, name=None):
@@ -10,7 +9,6 @@ def deploy(registry, image, name=None):
     k8s_deploy(image, name)
 
 def lambda_handler(event, context):
-    config.load_kube_config()
     for record in event['Records']:
         msg = json.loads(record['Sns']['Message'])
         print(msg)
